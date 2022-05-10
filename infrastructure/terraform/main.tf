a2916f386af0682c35f67fefed98b17aaa98bcd4
Linus <devopsfundamentals2022@gmail.com>
2022-05-10 22:00:44 +0000

    test

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -31,32 +31,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
   allocation_method   = "Dynamic"
 }
 
-locals { 
-nsgrules = {
-    ssh = {
-      name                       = "SSH"
-      priority                   = 1001
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "22"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-    http = {
-      name                       = "HTTP"
-      priority                   = 1002
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "8080"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-  }
-}
+
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
