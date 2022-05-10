f579293030d5bd7f068ed46ac7cef8e72edaf1d6
Linus <devopsfundamentals2022@gmail.com>
2022-05-10 22:08:38 +0000

    test

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_network_security_rule" "testrules" {
   destination_port_range      = each.value.destination_port_range
   source_address_prefix       = each.value.source_address_prefix
   destination_address_prefix  = each.value.destination_address_prefix
-  resource_group_name         = azurerm_resource_group.myterraformnsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.myterraformnsg.name
 }
 
