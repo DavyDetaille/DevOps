3a71e9f62f10f239d491c1162a1e977bee8f4bb7
Linus <devopsfundamentals2022@gmail.com>
2022-05-10 22:01:58 +0000

    test

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
   name                = "myNetworkSecurityGroup"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
+}
 resource "azurerm_network_security_rule" "testrules" {
   for_each                    = local.nsgrules 
   name                        = each.key
